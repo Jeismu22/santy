1bbee565433976bbdbe817bba2abe3a3f4bdc60d
Jeismu22 <srodriguezr21@curnvirtual.edu.co>
2024-11-28 22:23:16 -0500

    Mi aplicación Django Girls, primer commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .forms import PostForm
-
-
 from .models import Post
 
 
